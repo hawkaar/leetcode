48b8e9f93b6c3466b92b4ea02d67244ec8866596
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-13 11:03:30 +0300

    Adding new file

---
+++ NumberPalindrom.java
@@ -0,0 +1,12 @@
+public class NumberPalindrom {
+    public static void main(String[] args) {
+        int x = 123;
+
+        System.out.println(checkNumberPalindrom(x));
+    }
+
+    private static boolean checkNumberPalindrom(int x) {
+        
+        return false;
+    }
+}
