10da235b53515fd3a095076e83e93b0a1058c98b
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-13 11:15:05 +0300

    finishing

--- NumberPalindrom.java
+++ NumberPalindrom.java
@@ -1,12 +1,26 @@
 public class NumberPalindrom {
     public static void main(String[] args) {
-        int x = 123;
+        int x = -12321;
 
         System.out.println(checkNumberPalindrom(x));
     }
 
     private static boolean checkNumberPalindrom(int x) {
-        
-        return false;
+
+        String s = x + "";
+        int end = s.length() - 1;
+        boolean isPal = false;
+        if (x < 0) {
+            return isPal;
+        }
+
+       for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(end--)) {
+                isPal = true;
+            }else{
+                return false;
+            }
+       }
+        return isPal;
     }
 }
