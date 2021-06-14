8f5b889943b30a4964ee0801de3395dc141c9086
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-14 11:49:35 +0300

    starting from scratch

--- MyAtoI.java
+++ MyAtoI.java
@@ -1,6 +1,6 @@
 public class MyAtoI {
     public static void main(String[] args) {
-        String s = "-91283472332";
+        String s = "21474836460";
 
         System.out.println(myAtoi(s));
     }
@@ -13,7 +13,7 @@ public class MyAtoI {
         if (s.isEmpty()) {
             return 0;
         }
-        while(s.charAt(i) == ' ' && i < s.length()){
+        while(i < s.length() && s.charAt(i) == ' '){
             i++;
         }
 
@@ -24,32 +24,34 @@ public class MyAtoI {
         if (s.charAt(i) == '-') {
             neg = true;
             i++;
+        }else if(s.charAt(i) == '+'){
+            i++;
         }
 
         while (i < s.length() && Character.isDigit(s.charAt(i))) {
-            num = num * 10 + Character.getNumericValue(s.charAt(i));
             
             if (neg) {
-                if (i !=s.length()-1 
+                if (i !=s.length() 
                 && 
                 ((num * -1) < Integer.MIN_VALUE/10 
                 || 
                 ((num * -1) == Integer.MIN_VALUE/10 
                 && 
-                s.charAt(i+1) < 8))) {
+                Character.getNumericValue(s.charAt(i+1)) > 8))) {
                     return Integer.MIN_VALUE;
                 }
             }else{
-                if (i !=s.length()-1 
+                if (i !=s.length() 
                 && 
-                (num < Integer.MAX_VALUE/10 
+                (num > Integer.MAX_VALUE/10 
                 || 
                 (num == Integer.MAX_VALUE/10 
                 && 
-                s.charAt(i+1) < 7))) {
+                Character.getNumericValue(s.charAt(i+1)) > 7))) {
                     return Integer.MAX_VALUE;
                 }
             }
+            num = num * 10 + Character.getNumericValue(s.charAt(i));
             i++;
         }
 
