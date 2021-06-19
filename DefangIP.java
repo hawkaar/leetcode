660a656cf2eb1a705898647d4ee17dbb870b2622
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-19 23:55:00 +0300

    stupid solution for defangIP

---
+++ DefangIP.java
@@ -0,0 +1,20 @@
+public class DefangIP {
+    public static void main(String[] args) {
+        String ip = "10.100.152.1";
+
+        System.out.println(defangIPaddr(ip));
+    }
+
+
+    public static String defangIPaddr(String address) {
+        String newIp = "";
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                newIp = newIp + "[.]";
+            }else newIp = newIp + address.charAt(i);
+        }
+
+
+        return newIp;
+    }
+}
