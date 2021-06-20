8e2bfd6e7e14eba01901119b797f52dda8af9f57
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-20 11:22:31 +0300

    checkPanagram is done

---
+++ checkPanagram.java
@@ -0,0 +1,27 @@
+public class checkPanagram {
+    public static void main(String[] args) {
+        String sentence = "abcdefghijklmnopqrstuvwxyz";
+
+        System.out.println(checkIfPangram(sentence));
+    }
+
+    public static boolean checkIfPangram(String sentence) {
+
+        int[] alphabetsUsed = new int[26];
+
+        if (sentence.length() < 26)
+            return false;
+
+        for (int i = 0; i < sentence.length(); i++) {
+            alphabetsUsed[sentence.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < alphabetsUsed.length; i++) {
+            if (alphabetsUsed[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
