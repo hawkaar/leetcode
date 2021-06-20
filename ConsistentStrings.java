0effc5e79e45a2c0b70d653fe63eb37ac4262f03
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-20 13:57:38 +0300

    ConsistentStrings work

---
+++ ConsistentStrings.java
@@ -0,0 +1,24 @@
+public class ConsistentStrings {
+    public static void main(String[] args) {
+        String[] words = {"ad","bd","aaab","baa","badab"};
+        String allowed = "ab"; 
+
+        System.out.println(countConsistentStrings(allowed, words));
+    }
+
+    public static int countConsistentStrings(String allowed, String[] words) {
+
+        int consistent = 0;
+        for (String string : words) {
+            boolean isValid = true;
+            for (char ch : string.toCharArray()) {
+                if (!allowed.contains(String.valueOf(ch))) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) consistent++;
+        }
+        return consistent;
+    }
+}
