f7f39ecb7124c09e87a274b63aa76cb9e85127da
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-20 11:24:01 +0300

    one line solution

--- checkPanagram.java
+++ checkPanagram.java
@@ -5,23 +5,28 @@ public class checkPanagram {
         System.out.println(checkIfPangram(sentence));
     }
 
+
     public static boolean checkIfPangram(String sentence) {
-
-        int[] alphabetsUsed = new int[26];
-
-        if (sentence.length() < 26)
-            return false;
-
-        for (int i = 0; i < sentence.length(); i++) {
-            alphabetsUsed[sentence.charAt(i) - 'a']++;
-        }
-
-        for (int i = 0; i < alphabetsUsed.length; i++) {
-            if (alphabetsUsed[i] == 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return sentence.chars().distinct().count()==26?true:false; 
     }
+
+    // public static boolean checkIfPangram(String sentence) {
+
+    //     int[] alphabetsUsed = new int[26];
+
+    //     if (sentence.length() < 26)
+    //         return false;
+
+    //     for (int i = 0; i < sentence.length(); i++) {
+    //         alphabetsUsed[sentence.charAt(i) - 'a']++;
+    //     }
+
+    //     for (int i = 0; i < alphabetsUsed.length; i++) {
+    //         if (alphabetsUsed[i] == 0) {
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
 }
