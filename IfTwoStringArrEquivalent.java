418ead01f856c98d4999193b390f257a3aefe4cc
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-20 13:43:21 +0300

    one line solution

--- IfTwoStringArrEquivalent.java
+++ IfTwoStringArrEquivalent.java
@@ -1,9 +1,6 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class IfTwoStringArrEquivalent {
     public static void main(String[] args) {
-        String[] word1 = {"ab", "cd"};
+        String[] word1 = {"ab", "c"};
         String[] word2 = {"a", "bc"};
 
         System.out.println(arrayStringsAreEqual(word1, word2));
@@ -11,38 +8,6 @@ public class IfTwoStringArrEquivalent {
 
 
     public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-
-        Queue<Character> q = new LinkedList<>();
-
-        for (int i = 0; i < word1.length; i++) {
-            for (int j = 0; j < word1[i].length(); j++) {
-                q.add(word1[i].charAt(j));
-            }
-        }
-
-        for (int i = 0; i < word2.length; i++) {
-            for (int j = 0; j < word2[i].length(); j++) {
-                
-                if (q.isEmpty()) {
-                    System.out.println("Queue is empty");
-                    return false;
-                }
-
-                char c = word2[i].charAt(j);
-                char cInQueue = q.poll();
-                if(c != cInQueue){
-                    System.out.println("does not match");
-                    return false;
-                }
-            }
-        }
-
-        if (!q.isEmpty()) {
-            System.out.println("Queue is not empty");
-            return false;
-        }
-
-        System.out.println("at the end");
-        return true;
+        return String.join("", word1).equals(String.join("", word2));
     }
 }
