2149db27769635f5c7980b0bc71641c7066f5d7e
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-20 10:52:28 +0300

    new file

---
+++ SplitStringBalance.java
@@ -0,0 +1,25 @@
+public class SplitStringBalance {
+    public static void main(String[] args) {
+        String s = "RRLLRLLRL";
+        System.out.println(balancedStringSplit(s));
+    }
+
+    public static int balancedStringSplit(String s) {
+        int count = 0;
+        int numOfBSS = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R') {
+                count++;
+            }else{
+                count--;
+            }
+
+            if (count == 0) {
+                numOfBSS++;
+            }
+        }
+
+        return numOfBSS;
+    }
+}
