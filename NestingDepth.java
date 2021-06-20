783f78238abd28d4bad03885881e87b0a38b7c56
Hawkar Ali <52099865+hawkaar@users.noreply.github.com>
2021-06-20 11:06:22 +0300

    renaming

--- NestingDepth.java
+++ NestingDepth.java
@@ -8,18 +8,19 @@ public class NestingDepth {
     private static int maxDepth(String s) {
 
         int maxDepth = 0;
-        int LBracket = 0;
+        int leftBracket = 0;
 
 
         for (int i = 0 ; i < s.length(); i++) {
-            
+
             if (s.charAt(i) == '(') {
-                LBracket++;
+                leftBracket++;
+                maxDepth = Integer.max(maxDepth, leftBracket);
             }else if (s.charAt(i) == ')') {
-                LBracket--;
+                leftBracket--;
             }
 
-            maxDepth = Integer.max(maxDepth, LBracket);
+            
         }
 
 
